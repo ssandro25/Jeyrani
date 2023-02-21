6218d094900fda0945c4d61f5a88bd29923059e4
ssandro25 <andro.misakiani@gmail.com>
2023-02-21 23:08:28 +0400

    update: ui

--- script.js
+++ script.js
@@ -21,65 +21,72 @@ const system = [stoneSystem, scissorsSystem, paperSystem]
 
 const result = document.getElementById('result')
 
-let success = `<span class="text-success">შენ მოიგე!</span>`
+const success = `<span class="text-success">შენ მოიგე!</span>`
 
-let lose = `<span class="text-danger">სამწუხაროდ წააგე!</span>`
+const lose = `<span class="text-danger">სამწუხაროდ წააგე!</span>`
 
-let draw = `<span class="text-secondary">ფრე...</span>`
+const draw = `<span class="text-secondary">ფრე...</span>`
+
+const choseStone = ' სისტემამ აირჩია ქვა' + `<i class="fa-solid fa-hand-back-fist ms-2 text-white"></i>`
+
+const choseScissors = ' სისტემამ აირჩია მაკრატელი' + `<i class="fa-solid fa-hand-scissors ms-2 text-white"></i>`
+
+const chosePaper = ' სისტემამ აირჩია ქაღალდი' + `<i class="fa-solid fa-hand ms-2 text-white"></i>`
+
 function userChoose(clicked_id) {
     let randomIndex = Math.floor(Math.random() * system.length);
 
     switch (clicked_id + system[randomIndex]) {
         case stoneUser + stoneSystem :
-            result.innerHTML = draw + ' სისტემამ აირჩია ქვა';
+            result.innerHTML = draw + choseStone;
             stoneSystemDiv.classList.add("chosen");
             scissorsSystemDiv.classList.remove("chosen");
             paperSystemDiv.classList.remove("chosen");
             break;
         case stoneUser + scissorsSystem :
-            result.innerHTML = success + ' სისტემამ აირჩია მაკრატელი';
+            result.innerHTML = success + choseScissors;
             scissorsSystemDiv.classList.add("chosen");
             stoneSystemDiv.classList.remove("chosen");
             paperSystemDiv.classList.remove("chosen");
             break;
         case stoneUser + paperSystem :
-            result.innerHTML = lose + ' სისტემამ აირჩია ქაღალდი';
+            result.innerHTML = lose + chosePaper;
             paperSystemDiv.classList.add("chosen");
             stoneSystemDiv.classList.remove("chosen");
             scissorsSystemDiv.classList.remove("chosen");
             break;
         case scissorsUser + stoneSystem :
-            result.innerHTML = lose + ' სისტემამ აირჩია ქვა';
+            result.innerHTML = lose + choseStone;
             stoneSystemDiv.classList.add("chosen");
             scissorsSystemDiv.classList.remove("chosen");
             paperSystemDiv.classList.remove("chosen");
             break;
         case scissorsUser + scissorsSystem :
-            result.innerHTML = draw + ' სისტემამ აირჩია მაკრატელი';
+            result.innerHTML = draw + choseScissors;
             scissorsSystemDiv.classList.add("chosen");
             stoneSystemDiv.classList.remove("chosen");
             paperSystemDiv.classList.remove("chosen");
             break;
         case scissorsUser + paperSystem :
-            result.innerHTML = success + ' სისტემამ აირჩია ქაღალდი';
+            result.innerHTML = success + chosePaper;
             paperSystemDiv.classList.add("chosen");
             stoneSystemDiv.classList.remove("chosen");
             scissorsSystemDiv.classList.remove("chosen");
             break;
         case paperUser + stoneSystem :
-            result.innerHTML = success + ' სისტემამ აირჩია ქვა';
+            result.innerHTML = success + choseStone;
             stoneSystemDiv.classList.add("chosen");
             scissorsSystemDiv.classList.remove("chosen");
             paperSystemDiv.classList.remove("chosen");
             break;
         case paperUser +  scissorsSystem :
-            result.innerHTML = lose + ' სისტემამ აირჩია მაკრატელი';
+            result.innerHTML = lose + choseScissors;
             scissorsSystemDiv.classList.add("chosen");
             stoneSystemDiv.classList.remove("chosen");
             paperSystemDiv.classList.remove("chosen");
             break;
         case paperUser + paperSystem :
-            result.innerHTML = draw + ' სისტემამ აირჩია ქაღალდი';
+            result.innerHTML = draw + chosePaper;
             paperSystemDiv.classList.add("chosen");
             stoneSystemDiv.classList.remove("chosen");
             scissorsSystemDiv.classList.remove("chosen");
